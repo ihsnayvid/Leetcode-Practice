e12177c33b46683edc0148987fb94d9ffb825a73
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-05-05 14:15:25 +0530

    Time: 6 ms (9.19%), Space: 7 MB (19.21%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.cpp
@@ -10,15 +10,14 @@ public:
             q1.push(x);
             return;
         }
+        q2.push(x);
         while(!q1.empty()){
             q2.push(q1.front());
             q1.pop();
         }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }        
+        queue<int> temp = q1;
+        q1 = q2;
+        q2 = temp;
     }
     
     int pop() {
