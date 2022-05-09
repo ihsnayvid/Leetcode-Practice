e2d8f163aa435b9440c54d8d03bf3983a08c289d
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-05-09 23:16:04 +0530

    Time: 6 ms (10.87%), Space: 6.5 MB (84.83%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-//         unordered_map<int,string> mp;
-//         mp[2] = "abc";
-//         mp[3] = "def";
-//         mp[4] = "ghi";
-//         mp[5] = "jkl";
-//         mp[6] = "mno";
-//         mp[7] = "pqrs";
-//         mp[8] = "tuv";
-//         mp[9] = "wxyz";
         vector<string> ans;
-        if(digits.size() == 0) return ans;
         if(digits.size() == 1){
             if(digits[0] == '2') return {"a", "b", "c"};
             else if (digits[0] == '3') return {"d", "e", "f"};
