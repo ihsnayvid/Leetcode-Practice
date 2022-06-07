fa78f2f289c08229f2dc616161bec639011a5663
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-06-07 18:19:47 +0530

    Time: 4 ms (49.07%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -4,14 +4,10 @@ public:
         int t = m+n-1, i = m-1, j = n-1;
         
         while(i>=0 and j>=0){
-            if(nums1[i] > nums2[j]){
-                nums1[t] = nums1[i];
-                i--;
-            }
-            else{
-                nums1[t] = nums2[j];
-                j--;
-            }
+            if(nums1[i] > nums2[j])
+                nums1[t] = nums1[i--];
+            else
+                nums1[t] = nums2[j--];             
             t--;
         }
         
