281d032841003b5292132bad250f451a66f4f4df
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-06-30 11:17:53 +0530

    Time: 18 ms (42.75%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -8,8 +8,12 @@ public:
         else med = (nums[n/2] + nums[n/2 - 1])/2;
         
         int ans = 0;
-        for(int i=0; i<n; i++)
-            ans += abs(med - nums[i]);
+//         for(int i=0; i<n; i++)
+//             ans += abs(med - nums[i]);
+//         return ans;
+        
+        for(int i=0; i<n/2; i++)
+            ans += abs(nums[i] - nums[n-1-i]);
         return ans;
     }
 };
