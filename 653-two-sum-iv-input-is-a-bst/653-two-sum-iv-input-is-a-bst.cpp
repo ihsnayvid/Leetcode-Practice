381491c68e0de1120ea7412121618a485b44cd16
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-07-15 14:44:22 +0530

    Time: 286 ms (5.01%), Space: 36.3 MB (93.89%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,22 +11,21 @@
  */
 class Solution {
 public:
-    vector<int> in;
-    void store(TreeNode* root){
-        if(!root) return;
-        store(root->left);
-        in.push_back(root->val);
-        store(root->right);
+    bool solve(TreeNode *left, TreeNode *right, int k){
+        if(!left or !right) return false;
+        
+        int sum = left->val + right->val;
+        if(left == right and sum == k)  return solve(left->left, right->right, k);
+        
+        if(sum == k) return true;
+        else if(sum < k)
+            return solve(left->right, right, k) or solve(left, right->right, k);
+        else
+            return solve(left->left, right, k) or solve(left, right->left, k);
     }
+    
     bool findTarget(TreeNode* root, int k) {
         if(!root) return false;
-        store(root);
-        int i = 0, j = in.size() - 1;
-        while(i<j){
-            if(in[i] + in[j] == k) return true;
-            else if(in[i] + in[j] > k) j--;
-            else i++;
-        }
-        return false;
+        return solve(root, root, k);
     }
 };
