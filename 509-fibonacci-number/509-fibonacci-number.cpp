6c7575871e0010fa540428734d1d1078cd98829b
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-07-20 16:02:35 +0530

    Time: 15 ms (31.27%), Space: 6 MB (39.74%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
-        vector<int> fib(n+1);
-        if(n<2) return n;
-        fib[0] = 0;
-        fib[1] = 1;
-        
-        for(int i=2; i<= n; i++)
-            fib[i] = fib[i-1] + fib[i-2];
-        
-        return fib[n];
+        if(n == 0 or n == 1) return n;
+        return fib(n-1) + fib(n-2);
     }
 };
