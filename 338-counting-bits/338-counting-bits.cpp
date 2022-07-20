81cf842acde29b45f9dd52a29c8ad2dd0acd9b24
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-07-20 19:04:14 +0530

    Time: 8 ms (60.01%), Space: 8.6 MB (21.01%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int count(int n){
+        int ans = 0;
+        while (n){
+            if (n & 1)
+                ans ++;
+            n >>= 1;
+        }
+        return ans;
+    }
+    vector<int> countBits(int n) {
+        vector<int> ans;
+        for(int i=0; i<=n; i++){
+            ans.push_back(count(i));            
+        }
+        return ans;
+    }
+};
