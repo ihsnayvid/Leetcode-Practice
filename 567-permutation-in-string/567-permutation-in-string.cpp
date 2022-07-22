88bb6482c3e2f35574002f6c001839ef21789f52
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-07-22 19:20:08 +0530

    Time: 20 ms (42.09%), Space: 8.2 MB (23.57%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,31 +2,22 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int k = s1.length();
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         for(auto s: s1)
             mp[s]++;
         
         int i = 0, j = 0;
         
-        map<char,int> mpp;
+        unordered_map<char,int> mpp;
         while(j<s2.length()){
+            mpp[s2[j]]++;
             if(j-i+1 == k){ 
-                mpp[s2[j]]++;            
-                j++;
                 if(mpp == mp) return true;
                 mpp[s2[i]]--;
                 if(mpp[s2[i]] == 0) mpp.erase(s2[i]);
                 i++;
             }
-            else {
-                mpp[s2[j]]++;            
-                j++;
-            }
-            // cout<<"\nmp---";
-            // for(auto x: mp) cout<<x.first<<":"<<x.second<<" "; 
-            // cout<<"\nmpp---";
-            // for(auto x: mpp) cout<<x.first<<":"<<x.second<<" ";            
-
+            j++;
         }
         
         return false;
