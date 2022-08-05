7894fd97436ed9cd4f4bcc67d4c1ce38f5bac46e
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-05 11:37:20 +0530

    Time: 29 ms (54.40%), Space: 26.9 MB (11.06%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    int utility(vector<vector<int>>& grid, int i, int j){
-        if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
-            return 0;
-        if(grid[i][j] == 0)
-            return 0;       
-        grid[i][j]=0;         
-            
-        return (1 + utility(grid, i+1, j) + utility(grid, i, j+1) + utility(grid, i-1, j) + utility(grid, i, j-1));
-    }    
+     
     
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        int ans = 0, x = 0;
-        for(int i=0; i<n;  i++)
-            for(int j=0; j<m; j++){
-                if(grid[i][j])
-                    x = utility(grid, i, j);                
-                ans = max(ans, x);
-            }
+        int r = grid.size(), c = grid[0].size();
+        int ans = 0;
+        int vis[r][c];
+        memset(vis,0,sizeof(vis));
         
-        return ans;
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(grid[i][j] == 1 and !vis[i][j]){
+                    int temp = 1;
+                    int dx[4] = {0,0,1,-1};
+                    int dy[4] = {1,-1,0,0};
+                    
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    vis[i][j] = 1;
+                    
+                    while(!q.empty()){
+                        int x = q.front().first;
+                        int y = q.front().second;
+                        q.pop();
+                        
+                        for(int i=0; i<4; i++){
+                            int nextx = x + dx[i];
+                            int nexty = y + dy[i];
+                            if(nextx < 0 or nextx >=r or nexty < 0 or nexty >=c) continue;
+                            if(grid[nextx][nexty] == 1 and !vis[nextx][nexty]){
+                                temp ++;
+                                q.push({nextx, nexty});
+                                vis[nextx][nexty] = 1;
+                            }
+                        }
+                        
+                    }
+                    ans = max(ans, temp);
+                }
+            }
+        }
+        return ans;    
     }
 };
