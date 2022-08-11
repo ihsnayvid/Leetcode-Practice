39c6bd9445496ce573b0199649562865017124fc
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-11 15:46:17 +0530

    Time: 87 ms (75.21%), Space: 48.4 MB (44.36%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -2,19 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> dp(n+1, 0);
-        dp[n-1] = true;
-        for(int i=n-1; i>=0; i--){
-            for(int j=1; j<=nums[i] and i+j < n; j++)
-                if(dp[i+j]){
-                    dp[i] = true;
-                    break;
-                }                    
+        int jumps = 0;
+        for(int i=n-2; i>=0; i--){
+            jumps++;
+            if(nums[i] >= jumps)
+                jumps = 0;
         }
-        
-        // for(auto i:dp)
-        //     cout<<i<<" ";
-        // cout<<endl;   
-        return dp[0];
+        return !jumps;
     }
 };
