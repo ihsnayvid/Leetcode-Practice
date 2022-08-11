eb82dcb7d6e4a946fa229f3af027903bdb81517a
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-11 16:58:58 +0530

    Time: 515 ms (29.31%), Space: 17.2 MB (28.09%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1, 0);
-        
+        vector<int> dp(n+1, 10000);
+        dp[n-1] = 0;
+        dp[n] = 0;
         for(int i=n-2; i>=0; i--){
-            int minCost = 10000;
-            for(int j=1; j<=nums[i] and i+j <= n; j++){
-                minCost = min(minCost, dp[i+j]);
+            for(int j=1; j<=nums[i] and i+j < n; j++){
+                dp[i] = min(dp[i], 1 + dp[i+j]);
             }
-            dp[i] = minCost + 1;
         }
         // for(auto i: dp)
         //     cout<<i<<" ";
