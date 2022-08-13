6fba805d830655f57e47a2b056992ead59086dae
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-13 22:41:23 +0530

    Time: 1680 ms (15.47%), Space: 392.6 MB (5.02%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.cpp
@@ -5,7 +5,7 @@ public:
         for(int j = 0; j < s.size(); j += w){
             string word = s.substr(j,w);
             if(mp.find(word) != mp.end()){
-                if(mp[word]-- <= 0)
+                if(mp[word]-- == 0)
                     return false;
             }
             else return false;   
