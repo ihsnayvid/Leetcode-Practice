b665dd7092a880143b346dbb49a5be9844484a3a
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-14 15:46:10 +0530

    Time: 45 ms (73.27%), Space: 14.2 MB (21.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int r = grid.size(), c = grid[0].size();
-        int ans = 0;
-        int vis[r][c];
-        memset(vis,0,sizeof(vis));
+        int n = grid.size(), m = grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(n,vector<int>(m,0));
         
-        for(int i=0; i<r; i++){
-            for(int j=0; j<c; j++){
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(grid[i][j] == '1' and !vis[i][j]){
+                    q.push({i,j});
+                    vis[i][j] = 1;
                     ans ++;
                     int dx[4] = {0,0,1,-1};
                     int dy[4] = {1,-1,0,0};
-                    
-                    queue<pair<int,int>> q;
-                    q.push({i,j});
-                    vis[i][j] = 1;
-                    
                     while(!q.empty()){
-                        int x = q.front().first;
-                        int y = q.front().second;
-                        q.pop();
-                        
-                        for(int i=0; i<4; i++){
-                            int nextx = x + dx[i];
-                            int nexty = y + dy[i];
-                            if(nextx < 0 or nextx >=r or nexty < 0 or nexty >=c) continue;
-                            if(grid[nextx][nexty] == '1' and !vis[nextx][nexty]){
-                                q.push({nextx, nexty});
-                                vis[nextx][nexty] = 1;
+                        // ans++;
+                        for(int i=0; i<q.size(); i++){
+                            auto x = q.front().first;
+                            auto y = q.front().second;
+                            q.pop();
+                            for(int k=0; k<4; k++){
+                                int nextx = x + dx[k], nexty = y + dy[k];
+                                if(nextx < 0 or nextx >= n or nexty < 0 or nexty >= m or grid[nextx][nexty] == '0')
+                                    continue;
+                                if(grid[nextx][nexty] == '1' and !vis[nextx][nexty]){
+                                    vis[nextx][nexty] = 1;
+                                    q.push({nextx,nexty});
+                                }                                    
                             }
                         }
-                        
                     }
                 }
             }
