9487430d8f32b4735a2758b88d5a583a00120912
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-24 18:53:00 +0530

    Time: 35 ms (49.24%), Space: 6 MB (29.00%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return fmod(log10(n)/log10(3), 1) == 0;
+        while(n > 0 and n%3 == 0)
+            n/=3;
+        return n==1;
+    
     }
 };
