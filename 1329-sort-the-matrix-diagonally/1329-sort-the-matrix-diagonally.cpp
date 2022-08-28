8d2ca34c88abe67484c646be74a8796157fba558
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-28 15:41:09 +0530

    Time: 10 ms (90.91%), Space: 9.2 MB (70.39%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        unordered_map<int,vector<int>> mp;
         vector<vector<int>> ans;
         int n = mat.size(), m = mat[0].size(), i, x, y, c;
-        
-        
-        
         for(int j=0; j<m; j++){
             if(j == 0) i = n-1;
             else i = 0;
@@ -17,8 +13,7 @@ public:
                     v.push_back(mat[x][y]);
                     x++, y++;
                 }
-                sort(v.begin(), v.end());
-                
+                sort(v.begin(), v.end());                
                 x = i, y = j, c = 0;
                 while(x<n and y<m){
                     mat[x][y] = v[c++];
@@ -26,42 +21,6 @@ public:
                 }
             }
         }
-        
-        // for(auto &i: mp){
-        //     sort(i.second.begin(), i.second.end());
-        // }
-        
-//         for(auto i: mp){
-//             for(auto v: i.second)
-//                 cout<<v<<" ";
-//             cout<<endl;
-//         }
-        
-        
-        // int start = n-m;
-//         for(int j=0; j<m; j++){
-//             if(j == 0) i = n-1;
-//             else i = 0;
-            
-//             // cout<<i<<" "<<j<<endl;
-//             for(; i>=0; i--){
-//                 vector<int> temp = mp[i-j];
-//                 sort(temp.begin(), temp.end());
-                
-//                 // for(auto v: temp) cout<<v<<" ";
-//                 // cout<<endl;
-//                 // cout<<"here " <<i<<" , "<<j<<":";
-//                 int c = 0;
-//                 x = i, y = j;
-//                 while(x < n and y < m){
-//                     // cout<<x<<" "<<y<<endl;
-//                     mat[x][y] = temp[c++];
-//                     x++, y++;
-//                 }
-//                 // start --;
-//             }
-//         }
-
         return mat;
     }
 };
