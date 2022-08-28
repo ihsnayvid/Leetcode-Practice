4c2bdc27fac54bbeb62a4c475192286044534c09
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-28 21:30:16 +0530

    Time: 31 ms (10.12%), Space: 12.5 MB (9.08%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    void solve(string &s, string &ds, int idx, int n){
+        if(ds.length() == n){
+            ans.push_back(ds);
+            return;
+        }
+        
+        for(int i=idx; i<n; i++){
+            if(isdigit(s[i])){
+                ds.push_back(s[i]);
+                solve(s,ds,i+1,n);
+                ds.pop_back();
+            }
+            else{
+                char c = s[i];
+                
+                if(islower(s[i])){
+                    ds.push_back(toupper(c));
+                    solve(s,ds,i+1,n);
+                    ds.pop_back();
+                }
+                else{
+                    ds.push_back(tolower(c));
+                    solve(s,ds,i+1,n);
+                    ds.pop_back();
+                }
+                ds.push_back(c);
+                solve(s,ds,i+1,n);
+                ds.pop_back();
+            }        
+        }
+    }
+    
+    vector<string> letterCasePermutation(string s) {
+        string ds = "";
+        solve(s,ds,0,s.length());
+        return ans;
+    }
+};
