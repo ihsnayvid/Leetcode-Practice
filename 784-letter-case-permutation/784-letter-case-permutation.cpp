719124e5aeb9f4b675871996ee936ed4ea2ff599
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-08-28 21:34:06 +0530

    Time: 12 ms (49.62%), Space: 12.1 MB (21.82%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     vector<string> ans;
     
-    void solve(string &s, string &ds, int idx, int n){
+    void solve(string &s, string &ds, int i, int n){
         if(ds.length() == n){
             ans.push_back(ds);
             return;
         }
         
-        for(int i=idx; i<n; i++){
-            if(isdigit(s[i])){
-                ds.push_back(s[i]);
-                solve(s,ds,i+1,n);
-                ds.pop_back();
-            }
-            else{
-                char c = s[i];
-                
-                ds.push_back(toupper(c));
-                solve(s,ds,i+1,n);
-                ds.pop_back();
-
-                ds.push_back(tolower(c));
-                solve(s,ds,i+1,n);
-                ds.pop_back();
-            }        
+        
+        if(isdigit(s[i])){
+            ds.push_back(s[i]);
+            solve(s,ds,i+1,n);
+            ds.pop_back();
         }
+        else{
+            char c = s[i];
+
+            ds.push_back(toupper(c));
+            solve(s,ds,i+1,n);
+            ds.pop_back();
+
+            ds.push_back(tolower(c));
+            solve(s,ds,i+1,n);
+            ds.pop_back();
+        }        
+
     }
     
     vector<string> letterCasePermutation(string s) {
