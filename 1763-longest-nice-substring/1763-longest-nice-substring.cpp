f1807dc33c583d479ec95e32a77bfdbb37d46261
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-05 22:26:19 +0530

    Time: 21 ms (50.00%), Space: 12.5 MB (42.65%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.cpp
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    bool isNice(string s){
-        unordered_map<char, int> mp;
-        for(auto i: s)
-            mp[i]++;
-    
-        for(auto i: s){
-            if(isupper(i)){
-                // cout<<i<<" " << tolower(i)<<endl;
-                if(mp.find(tolower(i)) == mp.end()) return false;
-            }
-            else{
-                // cout<<i<<" "<<toupper(i)<<endl;
-                if(mp.find(toupper(i)) == mp.end()) return false;
-            }
-        }
-        return true;
-    }
-    
     string longestNiceSubstring(string s) {
+        if(s.size() < 2) return "";
         
-        int l = 0;
-        string ans = "";
-         
-        
+        set<char> st(s.begin(), s.end());
         for(int i=0; i<s.size(); i++){
-            for(int j=0; j<=s.size() - i; j++){
-                string sub = s.substr(i,j);
-                // cout<<sub<<endl;
-                if(isNice(sub) and sub.length() > ans.length())
-                    ans = sub;
+            if(!st.count(tolower(s[i])) or !st.count(toupper(s[i]))){
+                string first = longestNiceSubstring(s.substr(0,i));
+                string second = longestNiceSubstring(s.substr(i+1));
+                if(first.size() >= second.size()) return first;
+                return second;
             }
         }
-        return ans;
+        return s;
     }
 };
