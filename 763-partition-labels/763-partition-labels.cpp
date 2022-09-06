3449be2b0fe0eee97077776422654f45270f10b4
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-06 20:27:46 +0530

    Time: 7 ms (52.39%), Space: 6.9 MB (23.57%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int> mp;
+        unordered_map<char, int> mp;    //map to store last index of each character
+        int last = -1, mx = INT_MIN;
+        vector<int> ans;
+        
         for(int i=0; i<s.size(); i++)
             mp[s[i]] = i;
-        
-        vector<int> ans;
-        int last = -1, mx = INT_MIN;            
-
+            
         for(int i=0; i<s.size(); i++){
-            mx = max(mx, mp[s[i]]);
-            if(mx == i){
-                ans.push_back(i - last);
+            mx = max(mx, mp[s[i]]); //stores the farthest index of current partition characters
+            if(mx == i){            //current idx is the max till now
+                ans.push_back(i-last);
                 last = i;
             }
         }
