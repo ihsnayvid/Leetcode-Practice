2fc00762b8faa0f351f63aa54ebacd33387d7436
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-07 20:03:56 +0530

    Time: 57 ms (58.03%), Space: 20.8 MB (41.19%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans;
-        // vector<string> s = strs;    //copy of strs
         unordered_map<string, vector<string>> mp;
         
         for(int i=0; i<strs.size(); i++){
