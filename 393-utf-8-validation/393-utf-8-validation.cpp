985bdcd1579cb2a9ffa1ad5befb272ed83686b9f
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-13 15:58:50 +0530

    Time: 35 ms (15.46%), Space: 13.8 MB (91.77%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -3,10 +3,10 @@ public:
     bool validUtf8(vector<int>& data) {
         int c = 0;
         for(auto i: data){
-            if(!c){
-                if((i >> 5) == 0b110) c = 1; //2 bit
-                else if((i >> 4) == 0b1110) c = 2; //3 bit
-                else if((i >> 3) == 0b11110) c = 3; //4 bit
+            if(!c){ //first byte/ int
+                if((i >> 5) == 0b110) c = 1; //2 bit->check next 1 bit
+                else if((i >> 4) == 0b1110) c = 2; //3 bit->check next 2 bit
+                else if((i >> 3) == 0b11110) c = 3; //4 bit->check next 3 bit
                 else if((i >> 7)) return false; // 1 bit but first digit not zero
             }
             else{
