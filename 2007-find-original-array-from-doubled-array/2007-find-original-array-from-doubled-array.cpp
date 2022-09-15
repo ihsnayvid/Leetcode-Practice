f952791ff15a2eb8e8fa894b6a7635d8def0214c
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-15 15:22:46 +0530

    Time: 1038 ms (12.99%), Space: 149.9 MB (25.85%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -3,29 +3,17 @@ public:
     
     
     vector<int> findOriginalArray(vector<int>& changed) {
-        int n = changed.size();
-        
-        
         unordered_map<int, int> mp;
         sort(changed.rbegin(), changed.rend());
         vector<int> ans;
-        
-        // 8 6 4 3 2 1
-        // 
-        for(auto i: changed){           
-            
+         
+        for(auto i: changed){ 
             if(mp[2*i] > 0){
                 ans.push_back(i);
-
                 mp[2*i] --;
-                if(mp[2*i] <= 0) mp.erase(2*i);
             }
             else mp[i]++;            
         }
-        
-        // if(!mp.size()) return ans;
-        // return {};
-        // // return ans;
         
         for(auto i: mp){
             if(i.second != 0) return {};
