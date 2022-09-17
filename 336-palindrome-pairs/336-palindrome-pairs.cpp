5fe71455435fef70944dc7dbebdf46d12c19b805
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-09-17 21:53:49 +0530

    Time: 1741 ms (48.85%), Space: 516.2 MB (42.69%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -1,58 +1,49 @@
 class Solution {
-    bool check_palindrome(string &s)
-    {
-        int i=0;
-        int j = s.length()-1;
-        while(i<=j)
-        {
-            if(s[i++] != s[j--])
-            {
-                return false;
+public:
+    bool isPalindrome(string s){
+        int st = 0, en = s.length()-1;
+        while(st <= en){
+            if(s[st] != s[en]) return false;
+            st++, en --;
+        }
+        return true;
+    }
+
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        vector<vector<int>> ans;
+        int n = words.size();
+        if(n == 0) return ans;
+        
+        unordered_map<string, int> mp;
+        for(int i=0; i<n; i++){            
+            string rev = words[i];
+            reverse(rev.begin(), rev.end());            
+            mp[rev] = i;
+        }
+        
+        
+        for(int i=0; i<n; i++){
+            int l = words[i].length();
+                
+            string st = "", en = "";
+            for(int j=0; j<l; j++){
+                st += words[i][j];
+                en = words[i].substr(j+1);
+                 
+                if(mp.find(st) != mp.end() and mp[st] != i and isPalindrome(en))
+                    ans.push_back({i,mp[st]});
+                
+                if(mp.find(en) != mp.end() and mp[en] != i and isPalindrome(st))
+                    ans.push_back({mp[en],i});
             }
         }
-      return true;
-    }
-public:
-    vector<vector<int>> palindromePairs(vector<string>& words) {
-          vector<vector<int>> ans;
-          unordered_map<string,int> mp;
-          for(int i=0;i<words.size();i++)
-          {
-              string temp  = words[i];
-              reverse(temp.begin(),temp.end());
-              mp[temp] = i;
-          }
         
-          if(mp.find("") != mp.end())
-          {
-              for(int i=0;i<words.size();i++)
-              {
-                  if(!words[i].empty() && check_palindrome(words[i]))
-                  {
-                     ans.push_back({i,mp[""]});
-                  }
-              }
-          }
-        
-          for(int i=0;i<words.size();i++)
-          {
-              string left="",right="";
-              for(int j=0;j<words[i].size();j++)
-              {
-                  left.push_back(words[i][j]);
-                  right = words[i].substr(j+1);
-                 if(mp.count(left) && i != mp[left] && check_palindrome(right))
-                 {
-                     ans.push_back({i,mp[left]});
-                 }
-                  
-                  if(mp.count(right) && i!=mp[right] && check_palindrome(left))
-                  {
-                      ans.push_back({mp[right],i});
-                  }
-                
-              } 
-          }
-    return ans;
+        if(mp.find("") != mp.end()){
+            for(int i=0;i<words.size();i++){
+                if(!words[i].empty() and isPalindrome(words[i]))                  
+                    ans.push_back({i,mp[""]});                  
+            }
+        }
+        return ans;
     }
 };
