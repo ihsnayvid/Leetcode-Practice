7f6cfab546a94377680d981672165b1b3e2211f6
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-04 13:33:05 +0530

    Time: 6 ms (17.46%), Space: 7.8 MB (25.96%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    
-    
-//     int solve(vector<int> &v, int idx, int n, int sum){
-//         if(idx >= n){
-//             return sum;
-//         }
+    int solve(int idx, vector<int> &nums, vector<int> &dp){
+        if(idx < 0) return 0;
+        if(idx == 0) return nums[idx];
+        if(dp[idx] != -1) return dp[idx];
         
-//         int s1 = solve(v, idx+2, n, sum + v[idx]);
-//         int s2 = solve(v, idx+3, n, sum + v[idx]);
-//         return max(s1,s2);
-//     }
-    
-//     int rob(vector<int>& nums) {
-//         int s1 = solve(nums,0,nums.size(), 0);
-//         int s2 = solve(nums,1,nums.size(), 0);
-//         return max(s1,s2);
-//     }
-    
+        int pick = nums[idx] + solve(idx-2, nums, dp);
+        int nonPick = solve(idx-1, nums, dp);
+        
+        return dp[idx] = max(pick, nonPick);
+    }
     int rob(vector<int>& nums){
         
         int n = nums.size();
-        if(n == 1) return nums[0];
-        for(int i=n-2; i>=0; i--){
-            int cell1 = (i+2 < n) ? nums[i+2] : 0;
-            int cell2 = (i+3 < n) ? nums[i+3] : 0;
-            
-            nums[i] += max(cell1, cell2);
-        }
-        
-        return max(nums[0], nums[1]);
+        vector<int> dp(n+1, -1);
+        return solve(n-1, nums, dp);
     }
 };
