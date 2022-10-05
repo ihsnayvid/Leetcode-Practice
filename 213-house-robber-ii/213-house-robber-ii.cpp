cf04330ed5ca7bc8077460267d98690cec0d7bba
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-05 20:06:40 +0530

    Time: 4 ms (40.75%), Space: 7.8 MB (63.55%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int solve(vector<int> &nums){
-        
+    int solve(vector<int> &nums){        
         int n = nums.size();
-        if(n == 0) return 0;
         if(n == 1) return nums[0];
-        for(int i=n-2; i>=0; i--){
-            int cell1 = (i+2 < n) ? nums[i+2] : 0;
-            int cell2 = (i+3 < n) ? nums[i+3] : 0;
-            
-            nums[i] += max(cell1, cell2);
-        }
-        
-        return max(nums[0], nums[1]);
+        vector<int> dp(n+1, 0);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for(int i=2; i<n; i++)
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2]);
+        return dp[n-1];
     }
     int rob(vector<int>& nums) {
-        if(nums.empty()) return 0;
         if(nums.size() == 1) return nums[0];
         vector<int> a(nums.begin(), nums.end() - 1);
         vector<int> b(nums.begin() + 1, nums.end());
-//         for(auto i: a) cout<<i<<" ";
-//         cout<<endl;
-//         for(auto i: b) cout<<i<<" ";
-//         cout<<endl;
-        
         return max(solve(a), solve(b));
     }
 };
