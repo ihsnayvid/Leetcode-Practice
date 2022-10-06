a55334de14819ab0f53a036b4af28ff212dca520
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-06 20:13:29 +0530

    Time: 11 ms (6.94%), Space: 7.4 MB (49.39%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -5,15 +5,13 @@ public:
         if(n < 3) return 0;
         
         int ans = 0, c = 0;
-        for(int i=0; i<n-2; i++){
-            if(2 * (nums[i+1]) == (nums[i] + nums[i+2]))    //i, i+1, i+2 in AP
-                c++; 
-            else {
-                ans += (c * (c+1)) / 2;     //taking every combination till i-1 
-                c = 0;                      //subarrays of size 3, 4, 5 and so on 
+        for(int i=1; i<n-1; i++){
+            if(2 * (nums[i]) == (nums[i-1] + nums[i+1])){
+                c++;
+                ans += c;
             }
+            else c = 0;
         }
-        if(c) ans += (c * (c+1)) / 2;   //if still diff remains
         return ans;
     }
 };
