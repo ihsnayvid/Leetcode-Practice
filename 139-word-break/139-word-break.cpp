83137ffd5397d4b753745dcd51f8dfb10ee5be9d
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-06 22:10:33 +0530

    Time: 7 ms (85.11%), Space: 8.7 MB (74.49%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_map<string, int> mp;
+    vector<int> dp;
+    bool solve(string s, int idx){
+        if(idx == s.length()) return true;
+        if(s.length() == 0) return true;
+        if(mp.count(s)) return true;
+        string first = "";
+        for(int i=idx; i<s.length(); i++){
+            first += s[i];
+            if(mp.count(first)){
+                int next = (dp[i] != -1 ? dp[i] : (dp[i] =  solve(s, i+1)));
+                if(next)
+                    return true;
+            } 
+        }
+        return false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        for(auto word: wordDict)
+            mp[word]++;
+        dp.resize(s.length() + 1, -1);
+        return solve(s, 0);
+    }
+};
