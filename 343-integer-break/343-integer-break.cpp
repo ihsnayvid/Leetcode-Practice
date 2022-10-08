9f8694ebf459656ff1fc1034cf4f9b1206de44cb
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-08 15:05:40 +0530

    Time: 5 ms (22.65%), Space: 6.3 MB (29.77%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -3,8 +3,18 @@ public:
     int integerBreak(int n) {
         if(n == 2) return 1;
         if(n == 3) return 2;
-        if(n % 3 == 0) return pow(3, n/3);
-        else if(n % 3 == 1) return 4 * pow(3, (n/3) - 1);
-        else return 2 * pow(3, n/3);
+        if(n == 4) return 4;
+        if(n == 5) return 6;
+        if(n == 6) return 9;
+        vector<int> dp(n+2, 1);
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 4;
+        dp[5] = 6;
+        dp[6] = 9;
+        for(int i=7; i<=n; i++){
+            dp[i] = 3 * dp[i-3];
+        }
+        return dp[n];
     }
 };
