15a79ff6b232d2b93516054123be780fe9ebac2c
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-10-11 15:00:02 +0530

    Time: 53 ms (72.48%), Space: 13.5 MB (49.23%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int ans = 1;
+        for(int i=0; i<points.size(); i++){
+            int x1 = points[i][0], y1 = points[i][1];
+            unordered_map<double, int> mp;
+            for(int j=i+1; j<points.size(); j++){
+                int x2 = points[j][0], y2 = points[j][1];
+                if(x1 == x2){
+                    mp[INT_MAX]++;
+                }
+                else{
+                    double slope = ((y2 - y1) * 1.0) / (x2 - x1);
+                    mp[slope]++;
+                }
+            }
+            for(auto i: mp){
+                // cout<<i.first << " - " << i.second <<"\n";
+                ans = max(ans, i.second + 1);
+            }
+            cout<<"---\n";
+        }
+        
+        return ans;    
+    }
+};
