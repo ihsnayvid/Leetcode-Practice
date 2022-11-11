3cd96a1e471c2140a089890520dfcd1dc80e44fd
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-11-11 19:35:22 +0530

    Added solution - LeetHub

---
+++ Primes sum - GFG/primes-sum.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+    
+    bool isPrime(int n){
+        if(n < 2) return false;
+        if(n == 2) return true;
+        
+        for(int i=2; i <= sqrt(n); i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+    
+    string isSumOfTwo(int n){
+        
+        for(int i=2; i <= n/2; i++){
+            if(isPrime(i) and isPrime(n - i)) return "Yes";
+        }
+        
+        return "No";
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
