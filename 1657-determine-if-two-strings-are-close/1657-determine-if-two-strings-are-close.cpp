ea2d1d979c1d83f42ec3e0de3ad1a31b6e21a5f0
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2022-12-02 12:22:41 +0530

    Time: 583 ms (5.09%), Space: 24.6 MB (5.39%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        if( n != m) return false;
+        string s1 = word1, s2 = word2;
+        sort(word1.begin(), word1.end());
+        sort(word2.begin(), word2.end());
+        if(word1 == word2) return true;
+        
+        map<char, int> mp1, mp2;
+        multiset<int> st1, st2;
+        unordered_set<char> ch1, ch2;
+        for(auto i: word1) mp1[i]++, ch1.insert(i);
+        for(auto i: mp1) st1.insert(i.second);
+        
+        for(auto i: word2) mp2[i]++, ch2.insert(i);
+        for(auto i: mp2) st2.insert(i.second);
+        
+        return st1 == st2 and ch1 == ch2;
+    }
+};
