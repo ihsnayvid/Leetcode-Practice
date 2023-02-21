284e15715ee184b588c78a77bbcc4afca11c01db
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2023-02-21 10:37:14 +0530

    Added solution - LeetHub

--- Fill the Matrix - GFG/fill-the-matrix.cpp
+++ Fill the Matrix - GFG/fill-the-matrix.cpp
@@ -9,36 +9,9 @@ using namespace std;
 
 class Solution{   
 public:
-    int dx[4] = {0, 0, 1, -1};
-    int dy[4] = {1, -1, 0, 0};
     
     int minIteration(int n, int m, int x, int y){    
-        int ans = 0, filled = 1;
-        x--, y--;
-        queue<pair<int, int>> q;
-        vector<vector<int>> vis(n, vector<int> (m, 0));
-        q.push({x, y});
-        vis[x][y] = 1;
-        while(!q.empty()){
-            int s = q.size(); 
-            while(s--){
-                int i = q.front().first;
-                int j = q.front().second;
-                q.pop();
-                
-                // if(filled == n*m) return ans;
-                for(int k=0; k<4; k++){
-                    int nx = i + dx[k], ny = j + dy[k];
-                    if(nx < 0 or ny < 0 or nx >= n or ny >= m or vis[nx][ny]) continue;
-                    vis[nx][ny] = 1;
-                    q.push({nx, ny});
-                    // filled++;
-                }
-            }
-            // cout<<filled<<"\n";
-            if(!q.empty()) ans++;
-        }
-        return ans;
+        return max(x-1, n-x) + max(y-1, m-y);
     }
 };
 
