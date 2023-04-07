2faebf9619fe1cc3f374b25300afde9c610ec4fe
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2023-04-07 09:42:29 +0530

    Added solution - LeetHub

---
+++ Add Minimum Characters - GFG/add-minimum-characters.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+
+    
+    int addMinChar(string str){    
+        int n = str.length();
+        int i = 0, j = n - 1, count = 0;
+        while(i <= j){
+            if(str[i] != str[j]){
+                count++;
+                i = 0;
+                j = n - 1 - count;
+            }
+            else{
+                i ++, j --;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string str;
+        cin >> str;
+        
+        Solution ob;
+        cout << ob.addMinChar(str) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
