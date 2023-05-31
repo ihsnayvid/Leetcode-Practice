0d2d6afe9d76f3108dee04942623dede3944d71b
Divyanshi Srivastava <70314876+ihsnayvid@users.noreply.github.com>
2023-05-31 17:54:00 +0530

    Added solution - LeetHub

---
+++ Frequency Game - GFG/frequency-game.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int LargButMinFreq(int arr[], int n) {
+        unordered_map<int, int> mp;
+        for(int i=0; i<n; i++)
+            mp[arr[i]]++;
+         
+        int freq = n, el = -1;   
+        for(int i=0; i<n; i++)
+            freq = min(freq, mp[arr[i]]);
+        for(int i=0; i<n; i++){
+            if(mp[arr[i]] == freq){
+                el = max(el, arr[i]);
+            }
+        }
+        return el;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    // Iterating over testcases
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+
+        cout << ob.LargButMinFreq(arr, n) << endl;
+    }
+}
+// } Driver Code Ends
